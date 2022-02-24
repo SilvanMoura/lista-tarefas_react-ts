93c100fdb0bac204388040280b7644042c501dd9
Silvan Moura <89323396+SilvanMoura@users.noreply.github.com>
2022-02-24 09:21:34 -0300

    Criando component ListItem, importando e inserindo em App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import {useState} from 'react';
 import * as C from './App.styles';
 import {Item} from './types/Item'
+import {ListItem} from './components/ListItem'
 
 let App = ()=>{
-  let [list, setList] = useState<Item[]>([]);
+  let [list, setList] = useState<Item[]>([
+    {id:1, name: 'comprar pão', done:false},
+    {id:2, name: 'comprar bolo', done:false}
+  ]);
 
   return(
     <C.Container>
       <C.Area>
         <C.Header>iNotes</C.Header>
+
+        {list.map( (item, index)=>{
+          <ListItem />
+        } )}
       </C.Area>
     </C.Container>
   );
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,5 @@
+export let ListItem = ()=>{
+    return(
+        <div>...</div>
+    );
+};
